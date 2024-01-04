dd9c16b5023062b19c591356be4e8b2ee5861823
phanphongspvp <phanphongspvp@gmail.com>
2024-01-04 17:29:52 +0700

    ui/ux stories

---
+++ client/src/components/Story.jsx
@@ -0,0 +1,16 @@
+function Story() {
+  return (
+    <div>
+      <div className="w-16 h-16 rounded-full p-p-story border-gray-100 border-solid border-2 flex items-center justify-center">
+        <img
+          className="w-14 h-14 object-cover rounded-full border-transparent"
+          src="https://anhcuoiviet.vn/wp-content/uploads/2022/11/avatar-dep-dang-yeu-nu-5.jpg"
+          alt=""
+        />
+      </div>
+      <p className="text-xs text-gray-400 mt-1">phanphong...</p>
+    </div>
+  );
+}
+
+export default Story;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,22 @@
+import Story from "../components/Story";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-1 p-px-main">
+      <div className="w-width-content">
+        <div className="my-6 flex gap-4">
+          <Story />
+          <Story />
+          <Story />
+          <Story />
+          <Story />
+          <Story />
+        </div>
+        <div>Content</div>
+      </div>
+      <div className="pl-20">Sub</div>
+    </div>
+  );
 }
 
 export default Home;
